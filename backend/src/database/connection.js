d970ec31c047168ce48f471d64f43ac0264cf032
Johanquintero <quinteroquinte@hotmail.com>
2021-04-09 00:09:07 -0500

    push 3

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 async function connect() {
     try {
         const connection = mysql.createConnection({ 
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'softseguros'
+            host: 'ec2-107-22-245-82.compute-1.amazonaws.com',
+            user: 'kmltnuseqjfcbh',
+            password: '196825c894b791a34b05221997f6314a09eaa005da13c9f30b604a52add54e06',
+            database: 'df408bhcdn0p9f'
         });
 
         return connection;
--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,1 +1,1 @@
-export const apiURL = 'http://localhost:4000/api';
+export const apiURL = 'https://servidor-express.vercel.app/';
